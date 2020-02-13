42141a625e219e96f99989ac7042b4fb59cb76f2
David Tran <d.tran@samsung.com>
2020-02-13 14:13:03 -0800

    migrate to AndroidX

--- android/src/main/java/com/smartengineersinc/RNVideoPlayer/FullscreenVideoPlayerActivity.java
+++ android/src/main/java/com/smartengineersinc/RNVideoPlayer/FullscreenVideoPlayerActivity.java
@@ -6,7 +6,6 @@ import android.graphics.Color;
 import android.graphics.PixelFormat;
 import android.media.MediaPlayer;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
@@ -16,6 +15,8 @@ import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.Toast;
 import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class FullscreenVideoPlayerActivity extends AppCompatActivity {
     private String mVideoUrl;
